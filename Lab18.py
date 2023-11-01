215d4ec7779e97180f7f8509a5dea2a0eb12e698
kinziegrace4 <mwoodward@csumb.edu>
2023-11-01 12:47:53 -0700

    First commit of Lab 18

---
+++ Lab18.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+my_info = {
+    'title1': 'Blog 1',
+    'content1': 'This is the content of blog 1.',
+    'title2': 'Blog 2',
+    'content2': 'This is the content of blog 2.',
+}
+
+@app.route('/')
+def home():
+    return render_template('template1.html')
+
+@app.route('/page2')
+def p2():
+    return render_template('blog1.html', my_list=my_info)
+
+@app.route('/page3')
+def p3():
+    return render_template('blog2.html', my_list=my_info)
